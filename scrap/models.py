e6874ca560ab0d0355fc03b52e1825eb8528ca22
JackCX <jackcx777@gmail.com>
2020-12-22 16:29:22 +0300

    New DB table ProgrammingLanguage added and registered.

--- scrap/models.py
+++ scrap/models.py
@@ -4,9 +4,22 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название населенного пункта', unique=True)
     slug = models.CharField(max_length=50, blank=True, unique=True)
+
     class Meta():
-        verbose_name='Название населенного пункта'
+        verbose_name = 'Название населенного пункта'
         verbose_name_plural = 'Названия населенных пунктов'
+
     def __str__(self):
         return self.name
 
+
+class ProgrammingLanguage(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta():
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
