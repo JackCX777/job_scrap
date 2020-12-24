011b4d394b685b292375a8aa3f4919d614c35c68
JackCX <jackcx777@gmail.com>
2020-12-24 13:59:47 +0300

    In contrast to the previous GET request in google search, with a POST request the text of the request is not passed to the address bar. It can be used to hide data, e.g. logins and passwords.

--- scrap/views.py
+++ scrap/views.py
@@ -3,5 +3,6 @@ from .models import Vacancy
 
 
 def vacancy_view(request):
+    print(request.POST)
     query_set = Vacancy.objects.all()  # Request to database returns query_set object.
     return render(request, 'scrap/vacancy.html', {'object_list': query_set})
