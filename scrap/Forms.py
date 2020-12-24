9d9d22f3956c6f1879c11ed571f8e28849e08ce8
JackCX <jackcx777@gmail.com>
2020-12-24 16:39:46 +0300

    Search form view now more beautiful.

--- scrap/Forms.py
+++ scrap/Forms.py
@@ -3,5 +3,13 @@ from scrap.models import City, ProgrammingLanguage
 
 
 class FindForm(forms.Form):
-    city = forms.ModelChoiceField(queryset=City.objects.all())
-    programming_language = forms.ModelChoiceField(queryset=ProgrammingLanguage.objects.all())
+    city = forms.ModelChoiceField(queryset=City.objects.all(),
+                                  to_field_name="slug",
+                                  required=False,
+                                  widget=forms.Select(attrs={'class': 'form-control'}),
+                                  label='Город')
+    programming_language = forms.ModelChoiceField(queryset=ProgrammingLanguage.objects.all(),
+                                                  to_field_name="slug",
+                                                  required=False,
+                                                  widget=forms.Select(attrs={'class': 'form-control'}),
+                                                  label='Специальность')
