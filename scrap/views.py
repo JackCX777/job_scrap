6ebfcbed73118ec254f4d3ea6e988cd74d226b73
JackCX <jackcx777@gmail.com>
2020-12-24 15:03:41 +0300

    Search by name added.

--- scrap/views.py
+++ scrap/views.py
@@ -3,5 +3,14 @@ from .models import Vacancy
 
 
 def vacancy_view(request):
-    query_set = Vacancy.objects.all()  # Request to database returns query_set object.
+    city = request.GET.get('city')
+    programming_language = request.GET.get('programming_language')
+    query_set = []
+    if city or programming_language:
+        _filter = {}
+        if city:
+            _filter['city__name'] = city
+        if programming_language:
+            _filter['programming_language__name'] = programming_language
+        query_set = Vacancy.objects.filter(**_filter)  # Request to database returns query_set object.
     return render(request, 'scrap/vacancy.html', {'object_list': query_set})
