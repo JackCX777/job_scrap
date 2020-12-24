a434900074725cfc8d628d940b7c145cdb522324
JackCX <jackcx777@gmail.com>
2020-12-24 14:05:11 +0300

    Search form added.

--- scrap/views.py
+++ scrap/views.py
@@ -3,6 +3,5 @@ from .models import Vacancy
 
 
 def vacancy_view(request):
-    print(request.POST)
     query_set = Vacancy.objects.all()  # Request to database returns query_set object.
     return render(request, 'scrap/vacancy.html', {'object_list': query_set})
