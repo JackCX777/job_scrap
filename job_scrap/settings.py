f951905612e029b1e89a03d0d928190da9337b5f
JackCX <jackcx777@gmail.com>
2021-01-22 13:24:43 +0300

    python-dotenv installed for secure email address and password added in email settings for email sending (now email and pass in .env file). Don't forget to turn on risky applications supply in google account settings. email sending services are: mailgun.com and sendgrid.com free domain: freenom.com (only with 24/7 working site).

--- job_scrap/settings.py
+++ job_scrap/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,5 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'accounts.MyUser'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
