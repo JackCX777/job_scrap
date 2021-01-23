49ca0067f3a81973fa9492ad4d89f7d0f5dfc873
JackCX <jackcx777@gmail.com>
2021-01-23 17:10:22 +0300

    Url checking feature for pairs city and programming language added. If mails doesnt sands from django use smtplib.

--- send_emails.py
+++ send_emails.py
@@ -12,7 +12,7 @@ django.setup()
 
 from django.contrib.auth import get_user_model
 from django.core.mail import EmailMultiAlternatives
-from scrap.models import Vacancy, Error
+from scrap.models import Vacancy, Error, Url
 from job_scrap.settings import EMAIL_HOST_USER
 
 
@@ -23,6 +23,7 @@ subject = f'Рассылка вакансий за { today }'
 text_content = f'Рассылка вакансий за { today }'
 from_email = EMAIL_HOST_USER
 ADMIN_USER = EMAIL_HOST_USER
+
 query_set_usr = User.objects.filter(send_email=True).values('city', 'programming_language', 'email')
 users_dict = {}
 for _ in query_set_usr:
@@ -36,6 +37,7 @@ if users_dict:
         params['city_id__in'].append(pair[0])
         params['programming_language_id__in'].append(pair[1])
         # **params = unpack params dict, [:10] = temporary limit
+
     query_set_vac = Vacancy.objects.filter(**params, timestamp=today).values()
     # if .values() without any key, then keys + _id
     vacancies_dict = {}
@@ -56,17 +58,33 @@ if users_dict:
             msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
             msg.attach_alternative(_html, "text/html")
             msg.send()
+
 query_set_err = Error.objects.filter(timestamp=today)
+subject = ''
+text_content = ''
+_html = ''
+to = ADMIN_USER
+
 if query_set_err.exists():
     # my query set is the list of errors, and when I take first, only one error will be send.
     error = query_set_err.first()
     data = error.data
-    _html = ''
     for _ in data:
-        _html += f'<p><a href="{ _["url"] }">Error: { _["title"] }</a></p>'
+        _html += f'<p><a href="{ _["url"] }">Error: { _["title"] }</a></p><br>'
     subject = f'Ошибки сбора вакасий за { today }'
     text_content = f'Ошибки сбора вакасий за { today }'
-    to = ADMIN_USER
+
+query_set_url = Url.objects.all().values('city', 'programming_language')
+urls_dict = {(_['city'], _['programming_language']): True for _ in query_set_url}
+urls_pair_keys_error = ''
+for keys in users_dict.keys():
+    if keys not in urls_dict:
+        urls_pair_keys_error += f'<p>Для города { keys[0] } и специальности { keys[1] } нет url.</p><br>'
+if urls_pair_keys_error:
+    subject += ' Отсутствуют url'
+    _html += urls_pair_keys_error
+
+if subject:
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(_html, "text/html")
     msg.send()
@@ -80,3 +98,25 @@ if query_set_err.exists():
 # msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
 # msg.attach_alternative(html_content, "text/html")
 # msg.send()
+
+
+# If mails doesnt sands from django use smtplib
+
+# import smtplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+#
+# msg = MIMEMultipart('alternative')
+# msg['Subject'] = 'Список вакансий за  {}'.format(today)
+# msg['From'] = EMAIL_HOST_USER
+# mail = smtplib.SMTP()
+# mail.connect(EMAIL_HOST, 25)
+# mail.ehlo()
+# mail.starttls()
+# mail.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+#
+# html_m = "<h1>Hello world</h1>"
+# part = MIMEText(html_m, 'html')
+# msg.attach(part)
+# mail.sendmail(EMAIL_HOST_USER, [to], msg.as_string())
+# mail.quit()
